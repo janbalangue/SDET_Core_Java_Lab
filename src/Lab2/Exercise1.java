9ea3b52e0a54b47272e58167765c970cd558eacd
Jan Balangue <jan.a.balangue@gmail.com>
2022-06-28 17:24:04 -0500

    Add Lab2

---
+++ src/Lab2/Exercise1.java
@@ -0,0 +1,27 @@
+package Lab2;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		int num = 23;
+		boolean isCorrect = true;
+		String name = "James Gosling";
+		
+		printInt(num);
+		printIsCorrect(isCorrect);
+		printName(name);
+	}
+	
+	public static void printInt(int num) {
+		System.out.println("Number is " + num);
+	}
+	
+	public static void printIsCorrect(boolean isCorrect) {
+		System.out.println("isCorrect = " + isCorrect);
+	}
+	
+	public static void printName(String name) {
+		System.out.println("Name is " + name);
+	}
+
+}
