926b27498ebc844909d0810d1de6a8bb150b331c
Jan Balangue <jan.a.balangue@gmail.com>
2022-06-28 19:33:20 -0500

    Add L4E2

---
+++ src/lab4all/L4E2.java
@@ -0,0 +1,33 @@
+package lab4all;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
+public class L4E2 {
+	
+	public static void main(String[] args) {
+		
+		char letter = 'u';
+		
+		if (!Character.isLetter(letter)) {
+			System.out.println(letter + " is not a letter");
+			return;
+		}
+		
+		switch (letter) {
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+			System.out.println(letter + " is a vowel");
+			break;
+		case 'y':
+			System.out.println("y is sometimes a vowel");
+			break;
+		default:
+			System.out.println("letter is a consonant");
+		}
+		
+	}
+
+}
